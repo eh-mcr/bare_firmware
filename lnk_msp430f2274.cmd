63259f316eb5ae1a3721a6b3cc5447c9cfdaef4d
Ralph Stevenson-Jones <ralph.stevenson-jones@manchester.ac.uk>
2017-02-03 10:04:41 +0000

    updated lnk cmd file

--- lnk_msp430f2274.cmd
+++ lnk_msp430f2274.cmd
@@ -1,180 +1,54 @@
+/* ============================================================================ */
+/* Copyright (c) 2016, Texas Instruments Incorporated                           */
+/*  All rights reserved.                                                        */
+/*                                                                              */
+/*  Redistribution and use in source and binary forms, with or without          */
+/*  modification, are permitted provided that the following conditions          */
+/*  are met:                                                                    */
+/*                                                                              */
+/*  *  Redistributions of source code must retain the above copyright           */
+/*     notice, this list of conditions and the following disclaimer.            */
+/*                                                                              */
+/*  *  Redistributions in binary form must reproduce the above copyright        */
+/*     notice, this list of conditions and the following disclaimer in the      */
+/*     documentation and/or other materials provided with the distribution.     */
+/*                                                                              */
+/*  *  Neither the name of Texas Instruments Incorporated nor the names of      */
+/*     its contributors may be used to endorse or promote products derived      */
+/*     from this software without specific prior written permission.            */
+/*                                                                              */
+/*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" */
+/*  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,       */
+/*  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR      */
+/*  PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR            */
+/*  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,       */
+/*  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,         */
+/*  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; */
+/*  OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,    */
+/*  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR     */
+/*  OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,              */
+/*  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.                          */
+/* ============================================================================ */
+
 /******************************************************************************/
-/* lnk_msp430f2274.cmd - LINKER COMMAND FILE FOR LINKING MSP430F2274 PROGRAMS */
-/*                                                                            */
-/*  Ver | dd mmm yyyy | Who  | Description of changes                         */
-/* =====|=============|======|=============================================   */
-/*  0.01| 08 Mar 2004 | A.D. | First prototype                                */
-/*  0.02| 26 Mai 2004 | A.D. | Leading symbol underscores removed,            */
-/*      |             |      | Interrupt vector definition changed            */
-/*  0.03| 22 Jun 2004 | A.D. | File reformatted                               */
+/* lnk_msp430f2274.cmd - LINKER COMMAND FILE FOR LINKING MSP430F2274 PROGRAMS     */
 /*                                                                            */
 /*   Usage:  lnk430 <obj files...>    -o <out file> -m <map file> lnk.cmd     */
 /*           cl430  <src files...> -z -o <out file> -m <map file> lnk.cmd     */
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
-
 /* These linker options are for command line linking only.  For IDE linking,  */
 /* you should set your linker options in Project Properties                   */
 /* -c                                               LINK USING C CONVENTIONS  */
 /* -stack  0x0100                                   SOFTWARE STACK SIZE       */
 /* -heap   0x0100                                   HEAP AREA SIZE            */
-
+/*                                                                            */
 /*----------------------------------------------------------------------------*/
-/* 'Allocate' peripheral registers at given addresses                         */
+/* Version: 1.192                                                             */
 /*----------------------------------------------------------------------------*/
-
-/************************************************************
-* STANDARD BITS
-************************************************************/
-/************************************************************
-* STATUS REGISTER BITS
-************************************************************/
-/************************************************************
-* PERIPHERAL FILE MAP
-************************************************************/
-/************************************************************
-* SPECIAL FUNCTION REGISTER ADDRESSES + CONTROL BITS
-************************************************************/
-IE1                = 0x0000;
-IFG1               = 0x0002;
-IE2                = 0x0001;
-IFG2               = 0x0003;
-/************************************************************
-* ADC10
-************************************************************/
-ADC10DTC0          = 0x0048;
-ADC10DTC1          = 0x0049;
-ADC10AE0           = 0x004A;
-ADC10AE1           = 0x004B;
-ADC10CTL0          = 0x01B0;
-ADC10CTL1          = 0x01B2;
-ADC10MEM           = 0x01B4;
-ADC10SA            = 0x01BC;
-/************************************************************
-* Basic Clock Module
-************************************************************/
-DCOCTL             = 0x0056;
-BCSCTL1            = 0x0057;
-BCSCTL2            = 0x0058;
-BCSCTL3            = 0x0053;
-                                                                                    
-/*************************************************************
-* Flash Memory
-*************************************************************/
-FCTL1              = 0x0128;
-FCTL2              = 0x012A;
-FCTL3              = 0x012C;
-/************************************************************
-* Operational Amplifier
-************************************************************/
-OA0CTL0            = 0x00C0;
-OA0CTL1            = 0x00C1;
-OA1CTL0            = 0x00C2;
-OA1CTL1            = 0x00C3;
-/************************************************************
-* DIGITAL I/O Port1/2 Pull up / Pull down Resistors
-************************************************************/
-P1IN               = 0x0020;
-P1OUT              = 0x0021;
-P1DIR              = 0x0022;
-P1IFG              = 0x0023;
-P1IES              = 0x0024;
-P1IE               = 0x0025;
-P1SEL              = 0x0026;
-P1REN              = 0x0027;
-P2IN               = 0x0028;
-P2OUT              = 0x0029;
-P2DIR              = 0x002A;
-P2IFG              = 0x002B;
-P2IES              = 0x002C;
-P2IE               = 0x002D;
-P2SEL              = 0x002E;
-P2REN              = 0x002F;
-/************************************************************
-* DIGITAL I/O Port3/4 Pull up / Pull down Resistors
-************************************************************/
-P3IN               = 0x0018;
-P3OUT              = 0x0019;
-P3DIR              = 0x001A;
-P3SEL              = 0x001B;
-P3REN              = 0x0010;
-P4IN               = 0x001C;
-P4OUT              = 0x001D;
-P4DIR              = 0x001E;
-P4SEL              = 0x001F;
-P4REN              = 0x0011;
-/************************************************************
-* Timer A3
-************************************************************/
-TAIV               = 0x012E;
-TACTL              = 0x0160;
-TACCTL0            = 0x0162;
-TACCTL1            = 0x0164;
-TACCTL2            = 0x0166;
-TAR                = 0x0170;
-TACCR0             = 0x0172;
-TACCR1             = 0x0174;
-TACCR2             = 0x0176;
-/************************************************************
-* Timer B3
-************************************************************/
-TBIV               = 0x011E;
-TBCTL              = 0x0180;
-TBCCTL0            = 0x0182;
-TBCCTL1            = 0x0184;
-TBCCTL2            = 0x0186;
-TBR                = 0x0190;
-TBCCR0             = 0x0192;
-TBCCR1             = 0x0194;
-TBCCR2             = 0x0196;
-/************************************************************
-* USCI
-************************************************************/
-UCA0CTL0           = 0x0060;
-UCA0CTL1           = 0x0061;
-UCA0BR0            = 0x0062;
-UCA0BR1            = 0x0063;
-UCA0MCTL           = 0x0064;
-UCA0STAT           = 0x0065;
-UCA0RXBUF          = 0x0066;
-UCA0TXBUF          = 0x0067;
-UCA0ABCTL          = 0x005D;
-UCA0IRTCTL         = 0x005E;
-UCA0IRRCTL         = 0x005F;
-UCB0CTL0           = 0x0068;
-UCB0CTL1           = 0x0069;
-UCB0BR0            = 0x006A;
-UCB0BR1            = 0x006B;
-UCB0I2CIE          = 0x006C;
-UCB0STAT           = 0x006D;
-UCB0RXBUF          = 0x006E;
-UCB0TXBUF          = 0x006F;
-UCB0I2COA          = 0x0118;
-UCB0I2CSA          = 0x011A;
-/************************************************************
-* WATCHDOG TIMER
-************************************************************/
-WDTCTL             = 0x0120;
-/************************************************************
-* Calibration Data in Info Mem
-************************************************************/
-CALDCO_16MHZ       = 0x10F8;
-CALBC1_16MHZ       = 0x10F9;
-CALDCO_12MHZ       = 0x10FA;
-CALBC1_12MHZ       = 0x10FB;
-CALDCO_8MHZ        = 0x10FC;
-CALBC1_8MHZ        = 0x10FD;
-CALDCO_1MHZ        = 0x10FE;
-CALBC1_1MHZ        = 0x10FF;
-/************************************************************
-* Interrupt Vectors (offset from 0xFFE0)
-************************************************************/
-/************************************************************
-* End of Modules
-************************************************************/
 
 /****************************************************************************/
-/* SPECIFY THE SYSTEM MEMORY MAP                                            */
+/* Specify the system memory map                                            */
 /****************************************************************************/
 
 MEMORY
@@ -188,6 +62,7 @@ MEMORY
     INFOC                   : origin = 0x1040, length = 0x0040
     INFOD                   : origin = 0x1000, length = 0x0040
     FLASH                   : origin = 0x8000, length = 0x7FDE
+    BSLSIGNATURE            : origin = 0xFFDE, length = 0x0002, fill = 0xFFFF
     INT00                   : origin = 0xFFE0, length = 0x0002
     INT01                   : origin = 0xFFE2, length = 0x0002
     INT02                   : origin = 0xFFE4, length = 0x0002
@@ -207,42 +82,65 @@ MEMORY
 }
 
 /****************************************************************************/
-/* SPECIFY THE SECTIONS ALLOCATION INTO MEMORY                              */
+/* Specify the sections allocation into memory                              */
 /****************************************************************************/
 
 SECTIONS
 {
-    .bss       : {} > RAM                /* GLOBAL & STATIC VARS              */
-    .sysmem    : {} > RAM                /* DYNAMIC MEMORY ALLOCATION AREA    */
-    .stack     : {} > RAM (HIGH)         /* SOFTWARE SYSTEM STACK             */
+    .bss        : {} > RAM                  /* Global & static vars              */
+    .data       : {} > RAM                  /* Global & static vars              */
+    .TI.noinit  : {} > RAM                  /* For #pragma noinit                */
+    .sysmem     : {} > RAM                  /* Dynamic memory allocation area    */
+    .stack      : {} > RAM (HIGH)           /* Software system stack             */
 
-    .text      : {} > FLASH              /* CODE                              */
-    .cinit     : {} > FLASH              /* INITIALIZATION TABLES             */
-    .const     : {} > FLASH              /* CONSTANT DATA                     */
-    .cio       : {} > RAM                /* C I/O BUFFER                      */
+    .text       : {} > FLASH                /* Code                              */
+    .cinit      : {} > FLASH                /* Initialization tables             */
+    .const      : {} > FLASH                /* Constant data                     */
+    .bslsignature  : {} > BSLSIGNATURE      /* BSL Signature                     */
+    .cio        : {} > RAM                  /* C I/O Buffer                      */
 
-    .pinit     : {} > FLASH              /* C++ CONSTRUCTOR TABLES            */
+    .pinit      : {} > FLASH                /* C++ Constructor tables            */
+    .binit      : {} > FLASH                /* Boot-time Initialization tables   */
+    .init_array : {} > FLASH                /* C++ Constructor tables            */
+    .mspabi.exidx : {} > FLASH              /* C++ Constructor tables            */
+    .mspabi.extab : {} > FLASH              /* C++ Constructor tables            */
+#ifdef __TI_COMPILER_VERSION__
+  #if __TI_COMPILER_VERSION__ >= 15009000
+    #ifndef __LARGE_DATA_MODEL__
+    .TI.ramfunc : {} load=FLASH, run=RAM, table(BINIT)
+    #else
+    .TI.ramfunc : {} load=FLASH | FLASH2, run=RAM, table(BINIT)
+    #endif
+  #endif
+#endif
 
-    .infoA     : {} > INFOA              /* MSP430 INFO FLASH MEMORY SEGMENTS */
+    .infoA     : {} > INFOA              /* MSP430 INFO FLASH Memory segments */
     .infoB     : {} > INFOB
     .infoC     : {} > INFOC
     .infoD     : {} > INFOD
 
-    .int00   : {} > INT00                /* MSP430 INTERRUPT VECTORS          */
-    .int01   : {} > INT01
-    .int02   : {} > INT02
-    .int03   : {} > INT03
-    .int04   : {} > INT04
-    .int05   : {} > INT05
-    .int06   : {} > INT06
-    .int07   : {} > INT07
-    .int08   : {} > INT08
-    .int09   : {} > INT09
-    .int10   : {} > INT10
-    .int11   : {} > INT11
-    .int12   : {} > INT12
-    .int13   : {} > INT13
-    .int14   : {} > INT14
-    .reset   : {} > RESET              /* MSP430 RESET VECTOR               */ 
+    /* MSP430 Interrupt vectors          */
+    .int00       : {}               > INT00
+    .int01       : {}               > INT01
+    PORT1        : { * ( .int02 ) } > INT02 type = VECT_INIT
+    PORT2        : { * ( .int03 ) } > INT03 type = VECT_INIT
+    .int04       : {}               > INT04
+    ADC10        : { * ( .int05 ) } > INT05 type = VECT_INIT
+    USCIAB0TX    : { * ( .int06 ) } > INT06 type = VECT_INIT
+    USCIAB0RX    : { * ( .int07 ) } > INT07 type = VECT_INIT
+    TIMERA1      : { * ( .int08 ) } > INT08 type = VECT_INIT
+    TIMERA0      : { * ( .int09 ) } > INT09 type = VECT_INIT
+    WDT          : { * ( .int10 ) } > INT10 type = VECT_INIT
+    .int11       : {}               > INT11
+    TIMERB1      : { * ( .int12 ) } > INT12 type = VECT_INIT
+    TIMERB0      : { * ( .int13 ) } > INT13 type = VECT_INIT
+    NMI          : { * ( .int14 ) } > INT14 type = VECT_INIT
+    .reset       : {}               > RESET  /* MSP430 Reset vector         */
 }
+
+/****************************************************************************/
+/* Include peripherals memory map                                           */
+/****************************************************************************/
+
+-l msp430f2274.cmd
 
